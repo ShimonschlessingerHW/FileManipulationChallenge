b1d034e81d77f40da8f47f97d2f4e51402d13dc9
Shimon Schlessinger <sschlessinger@hwemail.com>
2025-09-15 12:43:20 -0700

    Refactored File Creation
    Pulled out 'make file' into its own function.

--- FileHandlingActivity.java
+++ FileHandlingActivity.java
@@ -1,6 +1,15 @@
 import java.io.*;
 
 public class FileHandlingActivity {
+    public static void createFile(String dirPath, String fileName){
+        File f = new File(dirPath + "/" + fileName);
+        try {
+            f.createNewFile();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         // Your code here
         
@@ -9,19 +18,12 @@ public class FileHandlingActivity {
         mainDir.mkdir();
 
         // b. Create three text files
-        File notes = new File("JavaFileSystem/notes.txt");
-        File data = new File("JavaFileSystem/data.txt");
-        File log = new File("JavaFileSystem/log.txt");
-        
-        try {
-            notes.createNewFile();
-            data.createNewFile();
-            log.createNewFile();
-        } catch (IOException e){
-
-        }
+        createFile("JavaFileSystem", "notes.txt");
+        createFile("JavaFileSystem", "log.txt");
+        createFile("JavaFileSystem", "data.txt");
     
         // c. Write messages to files
+
         
         // d. Read and display file contents
         
