a5f4fd0b59c0bc157d25080daec756b64f80c117
Shimon Schlessinger <sschlessinger@hwemail.com>
2025-09-15 12:39:55 -0700

    Part (b)
    Create three text files inside this directory: "notes.txt", "data.txt", and "log.txt"

--- FileHandlingActivity.java
+++ FileHandlingActivity.java
@@ -9,7 +9,18 @@ public class FileHandlingActivity {
         mainDir.mkdir();
 
         // b. Create three text files
+        File notes = new File("JavaFileSystem/notes.txt");
+        File data = new File("JavaFileSystem/data.txt");
+        File log = new File("JavaFileSystem/log.txt");
         
+        try {
+            notes.createNewFile();
+            data.createNewFile();
+            log.createNewFile();
+        } catch (IOException e){
+
+        }
+    
         // c. Write messages to files
         
         // d. Read and display file contents
